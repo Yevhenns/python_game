bd40d3032cc711980df9c71afb93507c42293327
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-20 21:30:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.constants import QUIT, K_DOWN
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 import random
 
 pygame.init()
@@ -8,13 +8,28 @@ screen = width, height = 800, 600
 
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
+RED = 255, 0, 0
 
 main_surface = pygame.display.set_mode(screen)
 
 ball = pygame.Surface((20, 20))
 ball.fill(WHITE)
 ball_rect = ball.get_rect()
-ball_speed = [1, 1]
+ball_speed = 1
+
+
+def create_enemy():
+    enemy = pygame.Surface((20, 20))
+    enemy.fill(RED)
+    enemy_rect = pygame.Rect(width, 100, *enemy.get_size())
+    enemy_speed = 1
+    return enemy, enemy_rect, enemy_speed
+
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+
+enemies = []
 
 is_working = True
 
@@ -22,14 +37,28 @@ while is_working:
     for event in pygame.event.get():
         if event.type == QUIT:
             is_working = False
-
-    ball_rect = ball_rect.move(ball_speed)
+        if event.type == CREATE_ENEMY:
+            enemies.append((create_enemy()))
 
     pressed_keys = pygame.key.get_pressed()
 
     main_surface.fill(BLACK)
-    main_surface.blit(ball, ball_rect)
 
-    if pressed_keys[K_DOWN]
+    main_surface.blit(ball, ball_rect)
+    main_surface.blit(enemy, enemy_rect)
+
+    if pressed_keys[K_DOWN]:
+        ball_rect = ball_rect.move(0, ball_speed)
+
+    if pressed_keys[K_UP]:
+        ball_rect = ball_rect.move(0, -ball_speed)
+
+    if pressed_keys[K_LEFT]:
+        ball_rect = ball_rect.move(-ball_speed, 0)
+
+    if pressed_keys[K_RIGHT]:
+        ball_rect = ball_rect.move(ball_speed, 0)
+
+    enemy_rect = enemy_rect.move(-enemy_speed, 0)
 
     pygame.display.flip()
